5806b896cd2cf1a0c2fad9586891c04b77b555e0
brooke perry and cole marsteller <brooke@>
2019-02-19 10:08:38 -0800

    add heart favorite functionality

---
+++ src/Styles/NewsItemStyles.jsx
@@ -0,0 +1,22 @@
+export const myStyledComponentStyles = {
+  border: '1px lightgrey solid',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+}
+export const imgStyle = {
+  borderRadius: '20px',
+  border: '3px white solid',
+  marginTop: '10px',
+  marginLeft: '10px',
+  marginRight: '10px',
+  backgroundColor: 'lightblue',
+  width: '40px'
+}
+export const itemImage = {
+  padding: '10px',
+
+}
+export const itemContent = {
+  padding: '10px',
+}
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,63 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
 import emptyHeart from "../assets/Heart_icon_red_hollow.svg";
+import filledHeart from "../assets/Heart_icon_red_filled.svg";
 
+import * as styles from "../Styles/NewsItemStyles";
 
-
-
-function NewsItem(props) {
-
-  let userName = '';
-  let content = '';
-
-  var myStyledComponentStyles = {
-    border: '1px lightgrey solid',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
-  var imgStyle = {
-    borderRadius: '20px',
-    border: '3px white solid',
-    marginTop: '10px',
-    marginLeft: '10px',
-    marginRight: '10px',
-    backgroundColor: 'lightblue',
-    width: '40px'
-  }
-  var itemImage = {
-    padding: '10px',
+class NewsItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props = props;
+    this.state = {
+      favoriteClicked: false
+    }
+    this.handleFavoriteClick = this.handleFavoriteClick.bind(this);
+    let userName = '';
+    let content = '';
 
   }
-  var itemContent = {
-    padding: '10px',
+
+  handleFavoriteClick(){
+    let favoriteClicked = this.state.favoriteClicked;
+    if (favoriteClicked === false) {
+      this.setState({favoriteClicked: true });
+    } else if (favoriteClicked === true) {
+      this.setState({favoriteClicked: false});
+    }
   }
 
-  return (
-    <div style={myStyledComponentStyles}>
-      <style jsx> {`
-          img {
-            max-width: 20px;
-          }
-    `}
-      </style>
-      <div style={itemImage}>
-        <img style={imgStyle} src="https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png"></img>
+
+  render() {
+    let heart;
+    if (this.state.favoriteClicked === false) {
+      heart = emptyHeart
+    } else if (this.state.favoriteClicked === true) {
+      heart = filledHeart
+    }
+    return (
+      <div style={styles.myStyledComponentStyles}>
+        <style jsx> {`
+            img {
+              max-width: 20px;
+            }
+      `}
+        </style>
+        <div style={styles.itemImage}>
+          <img style={styles.imgStyle} src="https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png"></img>
+        </div>
+        <hr/>
+        <div style={styles.itemContent}>
+          <p><em>User Name: {this.props.userName} </em></p>
+            <hr/>
+            <p>{this.props.content}</p>
+            <p>
+              <img onClick={this.handleFavoriteClick} src={heart}></img>
+            </p>
+        </div>
       </div>
-      <hr/>
-      <div style={itemContent}>
-        <p><em>User Name: {props.userName} </em></p>
-          <hr/>
-          <p>{props.content}</p>
-          <p>
-            <img src={emptyHeart}></img>
-          </p>
-      </div>
-    </div>
 
-  );
+    );
+  }
 }
-
 NewsItem.propTypes = {
   userName: PropTypes.string,
   content: PropTypes.string
